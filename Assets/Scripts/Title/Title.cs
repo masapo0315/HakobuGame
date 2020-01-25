adb3554475d8c9558f3dea82d9d305073a320a9b
高田将志 <masapo.0315@icloud.com>
2020-01-26 02:46:17 +0900

    タイトルシーンの作成

---
+++ Assets/Scripts/Title/Title.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour
+{
+    [SerializeField] private Text text;     //点滅させるテキスト
+    private float speed = 1.0f;
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetAlphaColor(text.color);
+    }
+
+    #region テキストの点滅
+
+    /// <summary>
+    /// Alhpa値を更新してColorを返す
+    /// </summary><param name= "color"></param>>
+    private void GetAlphaColor(Color color)
+    {
+        time += Time.deltaTime * 5.0f * speed;
+        color.a = Mathf.Sin(time) * 0.5f + 0.5f;
+
+        return;
+    }
+
+    #endregion
+
+}
