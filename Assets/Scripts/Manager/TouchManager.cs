a06ea0e73f713bcb9359f13301b3ff57041cc2ba
高田将志 <masapo.0315@icloud.com>
2020-01-26 20:07:27 +0900

    シーンマネージャーやタッチマネージャーを作ったりタイトルにロゴや背景を加えたり

---
+++ Assets/Scripts/Manager/TouchManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    #region シングルトン
+    private static TouchManager m_instance = null;
+
+    public static TouchManager Instnce
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                Debug.LogError("アタッチしているオブジェクトがありません");
+            }
+            return m_instance;
+        }
+    }
+
+    /// <summary>
+    /// シングルトン作成
+    /// </summary>
+    private void CreateInstnce()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this;
+            DontDestroyOnLoad(this.transform.gameObject);
+        }
+        else
+        {
+            Destroy(this.transform.gameObject);
+        }
+    }
+    #endregion
+
+    [HideInInspector] public bool touchFlag;      // タッチ有無
+    [HideInInspector] public Vector2 touchPosition;   // タッチ座標
+    [HideInInspector] public TouchPhase touchPhase;   // タッチ状態
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="flag">タッチしているかどうか</param>
+    /// <param name="position">タッチしている座標</param>
+    /// <param name="phase">タッチの状態</param>
+    public TouchManager(bool flag = false, Vector2? position = null, TouchPhase phase = TouchPhase.Began)
+    {
+        if(position == null)
+        {
+            this.touchPosition = new Vector2(0, 0);
+        } else {
+            this.touchPosition = (Vector2)position;
+        }
+        this.touchPhase = phase;
+    }
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        TouchEditor();
+    }
+
+    /// <summary>
+    /// エディタ
+    /// </summary>
+    private void TouchEditor()
+    {
+        this.touchFlag = false;
+
+        if (!Application.isEditor) return;
+
+        // 押した瞬間
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.touchFlag = true;
+            this.touchPhase = TouchPhase.Began;
+        }
+
+        // 離した瞬間
+        if (Input.GetMouseButtonUp(0))
+        {
+            this.touchFlag = true;
+            this.touchPhase = TouchPhase.Ended;
+        }
+
+        // 押しっぱなし
+        if (Input.GetMouseButton(0))
+        {
+            this.touchFlag = true;
+            this.touchPhase = TouchPhase.Moved;
+        }
+
+        //座標取得
+        if (this.touchFlag) this.touchPosition = Input.mousePosition;
+
+        //端末の場合
+        else
+        {
+            if (Input.touchCount == 0) return;
+
+            Touch touch = Input.GetTouch(0);
+            this.touchPosition = touch.position;
+            this.touchPhase = touch.phase;
+            this.touchFlag = true;
+        }
+    }
+
+    /// <summary>
+    /// タッチ状態取得
+    /// </summary>
+    public TouchManager getTouch()
+    {
+        return new TouchManager(this.touchFlag, this.touchPosition, this.touchPhase);
+    }
+}
--- Assets/Scripts/Title/Title.cs
+++ Assets/Scripts/Title/Title.cs
@@ -5,35 +5,43 @@ using UnityEngine.UI;
 
 public class Title : MonoBehaviour
 {
+    //private
+
+    [SerializeField] private float speed;
     [SerializeField] private Text text;     //点滅させるテキスト
-    private float speed = 1.0f;
     private float time;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //マネージャー
+
+    [SerializeField] SceneLoadManager _sceneLoadManager;    //SceneLoadManager
+    [SerializeField] TouchManager _touchManager;            //TouchManager
 
     // Update is called once per frame
     void Update()
     {
-        GetAlphaColor(text.color);
-    }
+        text.color = GetAlphaColor(text.color);
 
-    #region テキストの点滅
+        //タッチ取得
+        TouchManager touchState = _touchManager.getTouch();
+
+        //タッチされたらステージセレクトへ
+        if (!touchState.touchFlag) return;
+        if (touchState.touchPhase == TouchPhase.Began)
+        {
+            _sceneLoadManager.LoadScene("StageSelect");
+        }
+
+    }
 
     /// <summary>
     /// Alhpa値を更新してColorを返す
-    /// </summary><param name= "color"></param>>
-    private void GetAlphaColor(Color color)
+    /// </summary>
+    /// <param name= "color"></param>>
+    private Color GetAlphaColor(Color color)
     {
         time += Time.deltaTime * 5.0f * speed;
         color.a = Mathf.Sin(time) * 0.5f + 0.5f;
 
-        return;
+        return color;
     }
-
-    #endregion
-
 }
